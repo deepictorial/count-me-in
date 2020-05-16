b46912bb3f52f6d9c1edf526698130a9adba4f5b
lavendroid <5360592+deepictorial@users.noreply.github.com>
2020-05-15 22:56:04 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/www.deepictorial.be/count-me-in/precache-manifest.0c2bbe638614a0f32eaea3f0f5f15efe.js"
+  "/count-me-in/precache-manifest.41b513fb40d2a0ad8db72fb05a544c5b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/www.deepictorial.be/count-me-in/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/count-me-in/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
